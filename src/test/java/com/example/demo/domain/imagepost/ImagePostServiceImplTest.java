8c8773ca61bfd3fa10a276852caa76b13d89ffd3
Ngoc-Phuc Nguyen <77053108+Saizo24@users.noreply.github.com>
2022-11-22 13:34:57 +0100

    dynamic test with dummy test 1

---
+++ src/test/java/com/example/demo/domain/imagepost/ImagePostServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.example.demo.domain.imagepost;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImagePostServiceImplTest {
+
+  @Test
+  void updateById() {
+    assertTrue(true);
+  }
+}
